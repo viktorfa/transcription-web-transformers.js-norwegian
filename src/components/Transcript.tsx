b7d12a7745c54f2cb89139e154d169272f73529c
Viktor Andersen <vikfand@gmail.com>
2024-02-22 11:25:02 +0100

    Remove download json option

--- src/components/Transcript.tsx
+++ src/components/Transcript.tsx
@@ -95,12 +95,6 @@ export default function Transcript({ transcribedData }: Props) {
                     >
                         Last ned undertekster (.srt)
                     </button>
-                    <button
-                        onClick={exportJSON}
-                        className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 inline-flex items-center'
-                    >
-                        Last ned rådata (.json)
-                    </button>
                 </div>
             )}
         </div>
